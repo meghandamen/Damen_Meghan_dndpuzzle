49e6dc8de51eab840b75808acd6363f9c741a9fb
maggie <meghandamen@gmail.com>
2024-07-02 10:52:32 -0400

    transferred all my work from a offline file to github
    moved over the basic build we did in class to the github cloud file. Could not use github in the very beginning, have not changed any of the bugs yet

---
+++ js/main.js
@@ -0,0 +1,46 @@
+console.log("Javascript connected");
+
+//variables
+const theButtons = document.querySelectorAll("#buttonHolder img");
+const puzzleBoard = document.querySelector(".puzzle-board");
+const puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+const dropZones = document.querySelectorAll(".drop-zone");
+let draggedPiece;
+
+console.log(theButtons);
+console.log(puzzleBoard);
+
+//functions
+function changeBGImage() {
+    console.log("changeBGImage called");
+    console.log(this.id);
+    //background-image: url('../images/backGround0.jpg');
+    puzzleBoard.style.backgroundImage = `url('./images/backGround${this.id}.jpg')`;
+
+    //method 2
+    //console.log(evet.curremtTarget.id);
+    //puzzleBoard.style.backgroundImage = `url('./images/backGround${event.currentTarget.id}.jpg)`;
+}
+
+function handleStartDrag() {
+    console.log(`started dragging ${this}`);
+    draggedPiece = this;
+}
+
+function handleOver(e) {
+    console.log("dragged over")
+    e.preventDefault();
+}
+
+function handleDrop() {
+    this.appendChild(draggedPiece);
+}
+
+//event lisnerer
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+
+puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+
+dropZones.forEach(zone => zone.addEventListener("dragover", handleOver));
+
+dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
